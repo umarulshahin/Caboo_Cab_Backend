5dd8f006daea7e5ddb1d2b442a53924875c71e37
umarulshahin <akkushahin666@gmail.com>
2025-04-15 02:39:17 +0530

    fix: jwt access tocken time change

--- Caboo_backend/Caboo_backend/settings.py
+++ Caboo_backend/Caboo_backend/settings.py
@@ -204,7 +204,7 @@ CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 
 # JWT configuration
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),  
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),  
     "REFRESH_TOKEN_LIFETIME": timedelta(days=10),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
